d8a2dd4b359fe493b1f005b2cae7d6c604dbe301
Sipka Ognjen <ognjen105@gmail.com>
2020-06-04 11:13:25 +0200

    Updates

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,21 @@
 const path = require('path');
-const { alias } = require('react-app-rewire-alias');
+const { addBabelPresets, override, addBabelPlugins, addWebpackAlias } = require('customize-cra');
 
 module.exports = {
-    paths(paths, env) {
+    paths(paths) {
         paths.appIndexJs = path.resolve(__dirname, 'react/src/index.jsx');
         paths.appSrc = path.resolve(__dirname, 'react');
         return paths;
     },
-    webpack(config, env) {
-        alias({
-            '@core': 'react',
-        })(config);
+    webpack: override(
+        addWebpackAlias({
+            '@core': path.resolve(__dirname, 'react'),
+        }),
 
-        return config;
-    },
+        ...addBabelPresets('@babel/preset-env', '@babel/preset-react'),
+        ...addBabelPlugins(
+            '@babel/plugin-syntax-optional-chaining',
+            '@babel/plugin-proposal-object-rest-spread'
+        )
+    ),
 };
